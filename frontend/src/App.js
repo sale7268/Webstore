ba4bc9d30bd1f51fc7e9a9b95a58d419f86555c1
jerrykankelborg <jkankelborg@csumb.edu>
2020-10-08 16:43:43 -0700

    changed name to noodles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <header className="row">
             <div>
                 <a className="brand" href="/">
-                  Webstore for Legends
+                  Big noodles
                 </a>
             </div>
             <div>
