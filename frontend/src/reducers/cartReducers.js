e3f3846aa23a6b776f8fc6d26fc8f7dc8f1dcca5
jerrykankelborg <jkankelborg@csumb.edu>
2020-10-21 13:24:26 -0700

    paypal

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,9 @@
-import { CART_ADD_ITEM, CART_EMPTY, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
+import { 
+  CART_ADD_ITEM, 
+  CART_EMPTY, CART_REMOVE_ITEM, 
+  CART_SAVE_PAYMENT_METHOD, 
+  CART_SAVE_SHIPPING_ADDRESS, 
+} from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
