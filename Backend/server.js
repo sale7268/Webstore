2ced0306b1c462b45819d36f4e1647c1b15df415
sale7268 <hsaleem@csumb.edu>
2020-10-27 18:46:01 -0700

    dd

--- Backend/server.js
+++ Backend/server.js
@@ -28,9 +28,9 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/fronend/build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'));
+app.use(express.static(path.join(__dirname, '../fronend/build')));
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 
